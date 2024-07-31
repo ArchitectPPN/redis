0fe1be47777b9dc0a17bb30e1badc79125945e52
niujunqing <niujunqing@angelalign.com>
2024-07-31 10:45:35 +0800

    添加注释

--- src/replication.c
+++ src/replication.c
@@ -1921,24 +1921,25 @@ write_error: /* Handle sendSynchronousCommand(SYNC_CMD_WRITE) errors. */
 int connectWithMaster(void) {
     int fd;
 
-    fd = anetTcpNonBlockBestEffortBindConnect(NULL,
-        server.masterhost,server.masterport,NET_FIRST_BIND_ADDR);
+    // 连接master
+    fd = anetTcpNonBlockBestEffortBindConnect(NULL,server.masterhost,server.masterport,NET_FIRST_BIND_ADDR);
     if (fd == -1) {
         serverLog(LL_WARNING,"Unable to connect to MASTER: %s",
             strerror(errno));
         return C_ERR;
     }
 
-    if (aeCreateFileEvent(server.el,fd,AE_READABLE|AE_WRITABLE,syncWithMaster,NULL) ==
-            AE_ERR)
-    {
+    // 创建ae事件, 后续有消息时, 会触发syncWithMaster
+    if (aeCreateFileEvent(server.el,fd,AE_READABLE|AE_WRITABLE,syncWithMaster,NULL) == AE_ERR) {
         close(fd);
         serverLog(LL_WARNING,"Can't create readable event for SYNC");
         return C_ERR;
     }
 
+    // 更新
     server.repl_transfer_lastio = server.unixtime;
     server.repl_transfer_s = fd;
+    // 更新为连接中
     server.repl_state = REPL_STATE_CONNECTING;
     return C_OK;
 }
@@ -1978,9 +1979,7 @@ int cancelReplicationHandshake(void) {
     if (server.repl_state == REPL_STATE_TRANSFER) {
         replicationAbortSyncTransfer();
         server.repl_state = REPL_STATE_CONNECT;
-    } else if (server.repl_state == REPL_STATE_CONNECTING ||
-               slaveIsInHandshakeState())
-    {
+    } else if (server.repl_state == REPL_STATE_CONNECTING || slaveIsInHandshakeState()) {
         undoConnectWithMaster();
         server.repl_state = REPL_STATE_CONNECT;
     } else {
@@ -2579,26 +2578,32 @@ void replicationCron(void) {
     static long long replication_cron_loops = 0;
 
     /* Non blocking connection timeout? */
-    if (server.masterhost &&
-        (server.repl_state == REPL_STATE_CONNECTING ||
-         slaveIsInHandshakeState()) &&
-         (time(NULL)-server.repl_transfer_lastio) > server.repl_timeout)
+    if (
+            server.masterhost
+        && (server.repl_state == REPL_STATE_CONNECTING || slaveIsInHandshakeState())
+        && (time(NULL)-server.repl_transfer_lastio) > server.repl_timeout
+    )
     {
         serverLog(LL_WARNING,"Timeout connecting to the MASTER...");
         cancelReplicationHandshake();
     }
 
     /* Bulk transfer I/O timeout? */
-    if (server.masterhost && server.repl_state == REPL_STATE_TRANSFER &&
-        (time(NULL)-server.repl_transfer_lastio) > server.repl_timeout)
+    if (
+            server.masterhost
+        && server.repl_state == REPL_STATE_TRANSFER
+        && (time(NULL)-server.repl_transfer_lastio) > server.repl_timeout
+    )
     {
         serverLog(LL_WARNING,"Timeout receiving bulk data from MASTER... If the problem persists try to set the 'repl-timeout' parameter in redis.conf to a larger value.");
         cancelReplicationHandshake();
     }
 
     /* Timed out master when we are an already connected slave? */
-    if (server.masterhost && server.repl_state == REPL_STATE_CONNECTED &&
-        (time(NULL)-server.master->lastinteraction) > server.repl_timeout)
+    if (
+            server.masterhost && server.repl_state == REPL_STATE_CONNECTED
+        && (time(NULL)-server.master->lastinteraction) > server.repl_timeout
+    )
     {
         serverLog(LL_WARNING,"MASTER timeout: no data nor PING received...");
         freeClient(server.master);
@@ -2606,8 +2611,7 @@ void replicationCron(void) {
 
     /* Check if we should connect to a MASTER */
     if (server.repl_state == REPL_STATE_CONNECT) {
-        serverLog(LL_NOTICE,"Connecting to MASTER %s:%d",
-            server.masterhost, server.masterport);
+        serverLog(LL_NOTICE,"Connecting to MASTER %s:%d", server.masterhost, server.masterport);
         if (connectWithMaster() == C_OK) {
             serverLog(LL_NOTICE,"MASTER <-> REPLICA sync started");
         }
@@ -2616,8 +2620,11 @@ void replicationCron(void) {
     /* Send ACK to master from time to time.
      * Note that we do not send periodic acks to masters that don't
      * support PSYNC and replication offsets. */
-    if (server.masterhost && server.master &&
-        !(server.master->flags & CLIENT_PRE_PSYNC))
+    if (
+            server.masterhost
+        && server.master
+        && !(server.master->flags & CLIENT_PRE_PSYNC)
+    )
         replicationSendAck();
 
     /* If we have attached slaves, PING them from time to time.
